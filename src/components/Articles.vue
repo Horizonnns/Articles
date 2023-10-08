7de5a8ad9d232313fa87ee5a2e9d98b656004429
Kosimjon Kosimov <kosimjon.kosimov@megafon.tj>
2023-10-08 14:20:28 +0500

    Поправил роутинг на стр. пост

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,40 +1,21 @@
 <script setup>
-import earth from '../assets/images/articles/earth.jpg';
-import vscode from '../assets/images/articles/vscode.jpg';
 import { useRouter } from 'vue-router';
 const router = useRouter();
 
-const articles = [
-	{
-		id: 1,
-		tag: 'Javascript',
-		title:
-			'Как с помощью Dadata определить город по IP?',
-		descr:
-			'Делюсь информацией о том, как определить местоположение по IP',
-		img: earth,
-	},
-	{
-		id: 2,
-		tag: 'Советы',
-		title:
-			'Мои темы и плагины для VS Code на 2019 год',
-		descr:
-			'Так как спрашивают не в первый раз, сделаю отдельную запись со списком тем и плагинов',
-		img: vscode,
-	},
-];
+const props = defineProps({
+	articles: Array,
+});
 
 const changePage = (article) => {
-	if (article.tag) {
-		router.push('/ownarticle');
-	}
+	router.push(
+		`/post/${article.id}/${article.title}`
+	);
 };
 </script>
 
 <template>
 	<main
-		class="container grid md:grid-cols-2 justify-between md:space-x-8 space-y-8 md:space-y-0 py-6 md:py-10"
+		class="container grid md:grid-cols-2 justify-between md:space-x-8 space-y-8 md:space-y-0"
 	>
 		<div
 			v-for="article in articles"
